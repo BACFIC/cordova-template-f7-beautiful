3c32b13373c8fdc548987d32b6ee755c0e0ac754
Philippe Assis <assis@philippeassis.com>
2017-05-15 15:38:32 +0000

    my arc

---
+++ template_src/src/pages/app.vue
@@ -0,0 +1,112 @@
+<template lang="pug">
+// App
+#app
+	// Statusbar
+	f7-statusbar
+	// Left Panel
+	f7-panel(left='', reveal='', layout='dark')
+		f7-view#left-panel-view(navbar-through='', :dynamic-navbar='true')
+			f7-navbar(title='Left Panel')
+			f7-pages
+				f7-page
+					f7-block(inner='')
+						p Left panel content goes here
+					f7-block-title Load page in panel
+					f7-list
+						f7-list-item(link='/about/', title='About')
+						f7-list-item(link='/form/', title='Form')
+					f7-block-title Load page in main view
+					f7-list
+						f7-list-item(link='/about/', title='About', link-view='#main-view', link-close-panel='')
+						f7-list-item(link='/form/', title='Form', link-view='#main-view', link-close-panel='')
+	// Right Panel
+	f7-panel(right='', cover='', layout='dark')
+		f7-view#right-panel-view(navbar-through='', :dynamic-navbar='true')
+			f7-navbar(title='Right Panel', sliding='')
+			f7-pages
+				f7-page
+					f7-block
+						p Right panel content goes here
+					f7-block-title Load page in panel
+					f7-list
+						f7-list-item(link='/about/', title='About')
+						f7-list-item(link='/form/', title='Form')
+					f7-block-title Load page in main view
+					f7-list
+						f7-list-item(link='/about/', title='About', link-view='#main-view', link-close-panel='')
+						f7-list-item(link='/form/', title='Form', link-view='#main-view', link-close-panel='')
+	// Main Views
+	f7-views
+		f7-view#main-view(navbar-through='', :dynamic-navbar='true', main='')
+			// Navbar
+			f7-navbar
+				f7-nav-left
+					f7-link(icon='icon-bars', open-panel='left')
+				f7-nav-center(sliding='').custom Framework7
+				f7-nav-right
+					f7-link(icon='icon-bars', open-panel='right')
+			// Pages
+			f7-pages
+				f7-page
+					f7-block-title Welcome to my App
+					f7-block(inner='')
+						p
+							| Duis sed erat ac eros ultrices pharetra id ut tellus. Praesent rhoncus enim ornare ipsum aliquet ultricies. Pellentesque sodales erat quis elementum sagittis.
+					f7-block-title Navigation
+					f7-list
+						f7-list-item(link='/about/', title='About')
+						f7-list-item(link='/form/', title='Form')
+						f7-list-item(link='/dynamic-route/blog/45/post/125/?foo=bar#about', title='Dynamic Route')
+					f7-block-title Side Panels
+					f7-block
+						f7-grid
+							f7-col(width='50')
+								f7-button(open-panel='left') Left Panel
+							f7-col(width='50')
+								f7-button(open-panel='right') Right Panel
+					f7-block-title Modals
+					f7-block
+						f7-grid
+							f7-col(width='50')
+								f7-button(open-popup='#popup') Popup
+							f7-col(width='50')
+								f7-button(open-login-screen='#login-screen') Login Screen
+	// Popup
+	f7-popup#popup
+		f7-view(navbar-fixed='')
+			f7-pages
+				f7-page
+					f7-navbar(title='Popup')
+						f7-nav-right
+							f7-link(:close-popup='true') Close
+					f7-block
+						| Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, architecto. Cupiditate laudantium rem nesciunt numquam, ipsam. Voluptates omnis, a inventore atque ratione aliquam. Omnis iusto nemo quos ullam obcaecati, quod.
+	// Login Screen
+	f7-login-screen#login-screen
+		f7-view
+			f7-pages
+				f7-page(login-screen='')
+					f7-login-screen-title Login
+					f7-list(form='')
+						f7-list-item
+							f7-label Username
+							f7-input(name='username', placeholder='Username', type='text')
+						f7-list-item
+							f7-label Password
+							f7-input(name='password', type='password', placeholder='Password')
+					f7-list
+						f7-list-button(title='Sign In', close-login-screen='')
+						f7-list-label
+							p Click Sign In to close Login Screen
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus">
+	.custom
+		background -webkit-linear-gradient(#007AFF, #ffe600)
+		-webkit-background-clip text
+		-webkit-text-fill-color transparent
+</style>
--- template_src/webpack.config.js
+++ template_src/webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path'),
   fs = require('fs'),
-  
+
   webpack = require('webpack'),
   HtmlWebpackPlugin = require('html-webpack-plugin'),
   CordovaHtmlOutputPlugin = require('./webpack/plugins/CordovaHtmlOutputPlugin.js'),
   UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
   CleanPlugin = require('clean-webpack-plugin'),
   ExtractTextPlugin = require("extract-text-webpack-plugin"),
-  
-  entryFile = path.join(__dirname, 'src/main.js'),
+
+  entryFile = path.join(__dirname, 'src/app.js'),
   devServerPort = 8081
 
 let config = function (env) {
   let returner = {
     entry: entryFile,
-    
+
     resolve: {
       extensions: ['.js', '.json', '.vue'],
       modules: [path.join(__dirname, 'src'), 'node_modules'],
@@ -22,11 +22,14 @@ let config = function (env) {
         'vue$': 'vue/dist/vue.common.js',
         'src': path.resolve(__dirname, 'src/'),
         'assets': path.resolve(__dirname, 'src/assets/'),
-        'pages': path.resolve(__dirname, 'src/assets/vue/pages/'),
-        'components': path.resolve(__dirname, 'src/assets/vue/components/')
+        'pages': path.resolve(__dirname, 'src/pages/'),
+        'components': path.resolve(__dirname, 'src/components/'),
+        'styles': path.resolve(__dirname, 'src/styles/'),
+        'libs': path.resolve(__dirname, 'src/libs/'),
+        'config': path.resolve(__dirname, 'src/configs/')
       }
     },
-    
+
     output: {
       pathinfo: true,
       devtoolLineToLine: true,
@@ -34,18 +37,17 @@ let config = function (env) {
       sourceMapFilename: "[hash].[name].js.map",
       path: path.join(__dirname, 'www')
     },
-    
+
     module: {
       rules: [
         {test: /\.(png|jpe?g|gif)$/, loader: 'file-loader', options: {name: '[name].[ext]?[hash]'}},
         {test: /\.(woff2?|eot|ttf|otf|mp3|wav)(\?.*)?$/, loader: 'file-loader', options: {name: '[name].[ext]?[hash]'}},
         {test: /\.svg$/, loader: 'url-loader'},
-        {test: /\.scss$/, loader: [ 'vue-style-loader', 'css-loader', 'sass-loader']},
-        {test: /\.sass$/, loader: [ 'vue-style-loader', 'css-loader', 'sass-loader?indentedSyntax']},
+        {test: /\.styl$/, loader: [ 'vue-style-loader', 'css-loader', 'stylus-loader']},
         {test: /\.vue$/, loader: 'vue-loader'}
       ]
     },
-    
+
     plugins: [
       new webpack.DefinePlugin({
         'process.env': {
@@ -68,7 +70,7 @@ let config = function (env) {
       })
     ]
   }
-  
+
   if (typeof env === 'undefined' || typeof env.devserver === 'undefined') {
     returner.plugins.push(new CordovaHtmlOutputPlugin())
     returner.plugins.push(new ExtractTextPlugin("styles.css"))
@@ -79,7 +81,7 @@ let config = function (env) {
       })
     })
   }
-  
+
   if (env) {
     if (typeof env.devserver !== 'undefined' && env.devserver) {
       returner.module.rules.push({
@@ -115,7 +117,7 @@ let config = function (env) {
       returner.plugins.push(new UglifyJsPlugin())
     }
   }
-  
+
   return returner
 }
 
